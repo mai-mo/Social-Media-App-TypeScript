9d09688f3660300ae3c3005ac3fe78ab9299097f
Mai <mai-mo>
2026-03-31 07:11:51 +0200

    Week_14_3_Array_And_tuple

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
 let x = "Hello World";
 
 console.log(x);
+
+// array 
+const arr1: string[] = ["hi"]
+const arr2: string[] | number[] = [300]
+const arr3: (string | number | string[])[] = [300, ['red']]
+const arr4: readonly (string | number)[] = [300, 'red'];
+
+
+// tuples: is a ordered set can't modify it
+const grades: readonly [string, string, number] = ['A+', 'B+', 25]
+console.log(arr4)
