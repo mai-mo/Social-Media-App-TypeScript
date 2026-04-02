6b602518218ac35a1f2412cd5d9d428a975dd7b0
Mai <mai-mo>
2026-04-02 08:04:14 +0200

    Week14_7_Class_In_JS_VS_TS

---
+++ class in JS.js
@@ -0,0 +1,13 @@
+
+
+// class in Js
+class User {
+    _name;
+    constructor(name){
+        this._name = name
+    }
+}
+
+const userOne = new User("Mai")
+console.log(userOne)
+
---
+++ class in TS.ts
@@ -0,0 +1,11 @@
+
+// class in TS
+class User {
+    private name;
+    constructor(name: string){
+        this.name = name
+    }
+}
+
+const userOne = new User("Maha")
+console.log(userOne);
