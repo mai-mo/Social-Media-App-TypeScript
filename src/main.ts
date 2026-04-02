02bfbc8b1f1e67661a040e69304c1851b607a47b
Mai <mai-mo>
2026-04-02 06:34:53 +0200

    Week14_5_Function_And_Ts_options

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,34 @@
 let x = "Hello World";
 
+// Arrow Function
+const sum = (n1: number, n2: number): number => {
+    return (n1 + n2)
+}
+console.log(sum(5, 8))
 
-enum GenderEnum {
-    Male,
-    Female
-}
-type str = string
-type LastNameType = str | number
-type FullNameType = {
-    firstname: string;
-    lastname: LastNameType;
-}
-type UserType = {
-    username: FullNameType,
-    email: string,
-    age: number,
-    gender: GenderEnum,
-    confirm: boolean,
+function Calc({ n1, n2, n3 }: { n2?: number, n1: number, n3?: number }) {
+    return (n1 + (n2 || 0) * (n3 || 1))
 }
 
-let user: UserType & { password: string } = {
-    username: {
-        firstname: "Mai",
-        lastname: "Mohamed"
-    },
-    email: "mai@gmail.com",
-    age: 26,
-    gender: GenderEnum.Female,
-    confirm: true,
-    password: "Lolo"
-}
+console.log(Calc({ n1: 5, n2: 3 }))
 
-console.log(user.username);
 
-const colors = ["red color", "green color", 25, "black color"]
-for( let i = 0; i < colors.length; i++){
-    console.log((colors[i] as string).split(" "))
+// never 
+// function test(): never{
+//     while(true){
+
+//     }
+// }
+
+// console.log(test)
+// console.log("lol"); // مش هيوصل لها
+
+function test(n1: number, n2:any){
+    if(n1 > 25){
+        return "more than 25";
+    }
+    return n2;
 }
+console.log(test(5, 8));
+
 
