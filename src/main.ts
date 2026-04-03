f6796e12c009c2b0334d024c7a17881c0e50d0c2
Mai <mai-mo>
2026-04-03 16:15:57 +0200

    Week14_9_Interface_Implementation

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,22 @@
-let x = "Hello World";
 
-// Arrow Function
-const sum = (n1: number, n2: number): number => {
-    return (n1 + n2)
-}
-console.log(sum(5, 8))
-
-function Calc({ n1, n2, n3 }: { n2?: number, n1: number, n3?: number }) {
-    return (n1 + (n2 || 0) * (n3 || 1))
+interface IUser{
+    name: string,
+    DOB: number,
+    age: (currentYear: number)=> number
 }
 
-console.log(Calc({ n1: 5, n2: 3 }))
-
-
-// never 
-// function test(): never{
-//     while(true){
-
-//     }
-// }
-
-// console.log(test)
-// console.log("lol"); // مش هيوصل لها
-
-function test(n1: number, n2:any){
-    if(n1 > 25){
-        return "more than 25";
+const user: IUser = {
+    name: "Ahmed",
+    DOB: 2000,
+    age: function(this: IUser, currentYear: number) {
+        return currentYear - this.DOB;
     }
-    return n2;
 }
-console.log(test(5, 8));
 
+const user2:IUser = {
+    name: "Mai",
+    DOB: 2002,
+    
+} 
 
+console.log(user.age(2026))
