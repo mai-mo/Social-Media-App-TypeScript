e84d67962a85b7791a4bb6114dfa51bb302d4ef9
Mai <mai-mo>
2026-04-04 19:39:59 +0200

    Week14_11_InlineType_vs_type_Vs_Interfce_When_To_Use_Each_One

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,70 @@
+// TYPEs support primitive and non primitive
+// WITH TYPES 
+// type LoginType = {
+//     email: string,
+//     password: string
+// }
 
-interface IPerson {
-    name: string,
-    DOB: number,
-    age: (currentYear: number)=> number
+// type SignupType = LoginType & {
+//     username: string,
+// }
+
+// type UserType = SignupType & {
+//     id: number,
+// }
+
+// const users: UserType[] = [{ id: 1,username:"M", email: "m@gmail.com", password: "123" }]
+
+// const signup = ({email, password, username}: SignupType): UserType => {
+//     const user = { id: Date.now(),username, email, password }
+//     users.push(user)
+//     return user
+// }
+
+// const login = ({email, password}: LoginType): UserType | undefined=> {
+    
+//     const matchedAccount = users.find(user => {
+//         return user.email === email && user.password === password
+//     })
+//     return matchedAccount
+    
+// }
+
+// console.log(signup({email:"lol",username:"edd", password:"4878"}))
+// console.log(login({email:"lol", password:"4878"}))
+
+
+// Interface support non primitive
+
+interface ILogin {
+    email: string;
+    password: string;
 }
 
-interface IUser extends IPerson{
-    gender: number
+interface ISignup extends ILogin  {
+    username: string,
 }
 
-const user: Partial<IUser> = {
-    name: "Ahmed",
-    DOB: 2000,
-    age: function(this: IUser, currentYear: number) {
-        return currentYear - this.DOB;
-    },
+interface UserType extends ISignup {
+    id: number,
 }
 
+const users: UserType[] = [{ id: 1,username:"M", email: "m@gmail.com", password: "123" }]
 
-console.log((user as IUser).age(2026))
+const signup = ({email, password, username}: ISignup): UserType => {
+    const user = { id: Date.now(),username, email, password }
+    users.push(user)
+    return user
+}
+
+const login = ({email, password}: ILogin): UserType | undefined=> {
+    
+    const matchedAccount = users.find(user => {
+        return user.email === email && user.password === password
+    })
+    return matchedAccount
+    
+}
+
+console.log(signup({email:"lol",username:"edd", password:"4878"}))
+console.log(login({email:"lol", password:"4878"}))
