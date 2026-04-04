a63d0a98ce34d3e002842150fdd85e6c52bffdd6
Mai <mai-mo>
2026-04-04 20:25:20 +0200

    Week14_Part2_0_Class_Intro_And_Contructor_Short_hand

--- src/main.ts
+++ src/main.ts
@@ -1,70 +1,22 @@
-// TYPEs support primitive and non primitive
-// WITH TYPES 
-// type LoginType = {
-//     email: string,
-//     password: string
-// }
-
-// type SignupType = LoginType & {
-//     username: string,
-// }
-
-// type UserType = SignupType & {
-//     id: number,
-// }
-
-// const users: UserType[] = [{ id: 1,username:"M", email: "m@gmail.com", password: "123" }]
-
-// const signup = ({email, password, username}: SignupType): UserType => {
-//     const user = { id: Date.now(),username, email, password }
-//     users.push(user)
-//     return user
-// }
-
-// const login = ({email, password}: LoginType): UserType | undefined=> {
-    
-//     const matchedAccount = users.find(user => {
-//         return user.email === email && user.password === password
-//     })
-//     return matchedAccount
-    
-// }
-
-// console.log(signup({email:"lol",username:"edd", password:"4878"}))
-// console.log(login({email:"lol", password:"4878"}))
 
 
-// Interface support non primitive
+class Person{
 
-interface ILogin {
-    email: string;
-    password: string;
+    // Option 1
+    // public username: string;
+    // constructor(name: string) {
+    //     this.username = name
+    // }
+
+    // Option 2
+    constructor(public username: string){
+
+    }
+    getName(): string {
+        return `Hello ${this.username}`
+    }
+
 }
 
-interface ISignup extends ILogin  {
-    username: string,
-}
-
-interface UserType extends ISignup {
-    id: number,
-}
-
-const users: UserType[] = [{ id: 1,username:"M", email: "m@gmail.com", password: "123" }]
-
-const signup = ({email, password, username}: ISignup): UserType => {
-    const user = { id: Date.now(),username, email, password }
-    users.push(user)
-    return user
-}
-
-const login = ({email, password}: ILogin): UserType | undefined=> {
-    
-    const matchedAccount = users.find(user => {
-        return user.email === email && user.password === password
-    })
-    return matchedAccount
-    
-}
-
-console.log(signup({email:"lol",username:"edd", password:"4878"}))
-console.log(login({email:"lol", password:"4878"}))
+const person = new Person("ali");
+console.log(person.getName());
