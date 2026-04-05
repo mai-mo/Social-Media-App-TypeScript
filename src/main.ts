e50d0fa4106cb403fa0178bd7524eddac794d54f
Mai <mai-mo>
2026-04-05 08:18:56 +0200

    Week14_Part2_3_Association

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,20 @@
+class Clinic {
+    clinics: any[] = []
+    constructor(name: string, location: string){
+        this.clinics.push({name, location})
+    }
+    findOne(name: string){
+        const result = this.clinics.find(ele => ele.name == name)
+        return result
+    }
+}
+class Doctor{
+    constructor(public doctorName: string, public clinic: Clinic){
 
-class Person{
-    
-    private static _count: number = 0;
-
-    public get count(): number{
-        return Person._count
-    }
-
-    public get password(): string{
-        return this._password
-    }
-    public set password(value: string){
-        this._password = value
-    }
-    constructor(public username: string, private _password: string, protected phone: string){
-        Person._count++;
-    }
-    getName(): string{
-        return `Hello ${this.username}`
-    }
-    public updatePassword(value:string): void{
-        this._password = value
-    }
-    public getPassword(): string {
-        return this._password
     }
 }
 
-class User extends Person{
-    constructor( username:string, password: string, phone: string){
-        super(username, password, phone)
-    }
-}
+const clinic1 = new Clinic("TIBA", "Cairo")
+const doc1 = new Doctor("Ahmed", clinic1)
 
-
-const user = new User("M", "123", "0112")
-console.log(user)
-
-const person = new Person("A", "456", "010")
-console.log(person.count);
+console.log(doc1.clinic.findOne("TIBA"));
