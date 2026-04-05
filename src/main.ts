5bf25ee0e37534de7fce85f08f5420c1f0db6b3b
Mai <mai-mo>
2026-04-05 06:47:25 +0200

    Week14_Part2_2_Protected_And_Static_vs_Private_Static

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 
 class Person{
-    // public userName:string
-    // constructor(name: string){
-    //     this.userName = name
-    // },
-    // getName(): string{
-    // return `Hello, ${this.userName}`
-    // }
+    
+    private static _count: number = 0;
+
+    public get count(): number{
+        return Person._count
+    }
+
     public get password(): string{
         return this._password
     }
     public set password(value: string){
         this._password = value
     }
-    constructor(public username: string, private _password: string){
+    constructor(public username: string, private _password: string, protected phone: string){
+        Person._count++;
     }
     getName(): string{
         return `Hello ${this.username}`
@@ -22,19 +23,19 @@ class Person{
         this._password = value
     }
     public getPassword(): string {
-        return this.password
+        return this._password
     }
 }
 
 class User extends Person{
-    constructor( username:string, password: string){
-        super(username, password)
+    constructor( username:string, password: string, phone: string){
+        super(username, password, phone)
     }
 }
 
 
-const person = new Person("M", "123")
-console.log(person.getName())
-console.log(person.password);
-person.password= "xsd";
-console.log(person.password);
+const user = new User("M", "123", "0112")
+console.log(user)
+
+const person = new Person("A", "456", "010")
+console.log(person.count);
